3e287b0a82d78613b84d2b340581e9f29b7fff06
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-06-21 10:45:03 +0100

    Refactor code structure for improved readability and maintainability

---
+++ frontend/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className='w-full h-full max-w-[1440px] mx-auto'>
+      <header className="bg-[url('/images/header-bg.svg')] bg-center bg-cover bg-no-repeat h-[300px] lg:h-[913] p-12">
+        <h1 className='font-semibold text-lg lg:text-3xl'>RoomRadar</h1>
+      </header>
+    </div>
+  )
+}
