a18f818ac5ddab4fa6ce0908eaf3750cdbc51a05
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-06-23 15:22:26 +0100

    style: update HomePage layout and enhance text styles for better readability

--- frontend/components/HomePage.tsx
+++ frontend/components/HomePage.tsx
@@ -13,8 +13,7 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="w-full h-full">
-
-      <header className="bg-[url('/images/header-bg.svg')] bg-center bg-cover bg-no-repeat h-[400px] lg:h-screen flex flex-col relative lg:p-0 p-8 ">
+      <header className="bg-[url('/images/header-bg.svg')] bg-center bg-cover bg-no-repeat h-screen flex flex-col relative lg:p-0 p-8 lg:mb-[578px] mb-8">
         <motion.h1
           className="font-semibold text-xl lg:text-3xl absolute top-8 left-8 lg:left-[92px]"
           variants={fadeInUp}
@@ -40,7 +39,7 @@ export default function HomePage() {
           viewport={{ once: true, amount: 0.3 }}
         >
           <motion.p
-            className="font-bold text-[85px] flex-col hidden lg:flex mt-[231px] leading-[92px]"
+            className="font-bold lg:text-[85px] text-[32px] flex-col lg:mt-[231px] lg:leading-[92px] leading-[35px] mt-[321px]"
             variants={fadeInUp}
             initial="hidden"
             whileInView="visible"
@@ -49,16 +48,7 @@ export default function HomePage() {
             Find Your Perfect <br /> Off-Campus <br /> Home
           </motion.p>
           <motion.p
-            className="lg:hidden font-bold text-2xl mt-[7rem]"
-            variants={fadeInUp}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.3 }}
-          >
-            Find Your Perfect Off Campus Home
-          </motion.p>
-          <motion.p
-            className="italic lg:text-3xl text-base"
+            className="italic lg:text-3xl text-sm"
             variants={fadeIn}
             initial="hidden"
             whileInView="visible"
@@ -74,7 +64,7 @@ export default function HomePage() {
             viewport={{ once: true, amount: 0.3 }}
           >
             <motion.button
-              className="bg-[#00FF11] rounded-[10px] lg:text-[39px] font-bold text-xl py-3 px-6 hover:opacity-90 cursor-pointer"
+              className="bg-[#00FF11] rounded-[10px] lg:text-[39px] font-bold text-base py-3 px-6 hover:opacity-90 cursor-pointer"
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.97 }}
               variants={scaleIn}
@@ -85,7 +75,75 @@ export default function HomePage() {
         </motion.div>
       </header>
 
+      <motion.section
+        className="w-full p-8 lg:pl-[92px]"
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        <div className="flex flex-col">
+          <motion.p
+            className="font-bold text-lg mb-10 lg:text-[40px]"
+            variants={fadeInUp}
+          >
+            Wait
+          </motion.p>
+          <motion.p
+            className="font-bold text-2xl lg:text-[40px]"
+            variants={fadeInUp}
+          >
+            A <span className="text-[#00FF11]">Perfect</span> Off Campus Home??
+          </motion.p>
+          <motion.p className="font-bold" variants={fadeInUp}>
+            <span className="text-xl lg:text-[45px]">Without</span>{" "}
+            <span className="text-[#FFC403] text-[32px] lg:text-[90px]">
+              Stress??!
+            </span>
+          </motion.p>
+        </div>
+      </motion.section>
 
+      <motion.p
+        className="mt-[206px] lg:mt-[108px] text-center font-bold text-base lg:text-[40px] mb-[452px] lg:mb-[426px]"
+        variants={fadeIn}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        Is that possible
+      </motion.p>
+
+      <motion.section
+        className="p-8 lg:pl-[92px] max-w-[1390px]"
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        <div className="p-2 flex flex-col gap-4">
+          <motion.p
+            className="text-base font-medium lg:text-3xl"
+            variants={fadeInUp}
+          >
+            <span className="text-[#00FF11] font-semibold">Off-Campus</span>{" "}
+            life can feel like a maze of{" "}
+            <span className="text-[#FFC403] font-semibold">scams</span>,{" "}
+            <span className="text-[#FFC403] font-semibold">long walks</span>,
+            and{" "}
+            <span className="text-[#FFC403] font-semibold">hidden fees</span>.
+            All <span className="text-[#00FF11] font-semibold">Off-K</span> kids
+            all have one or two stories to tell, and some are worse than others.
+          </motion.p>
+          <motion.p className="font-medium lg:text-3xl" variants={fadeInUp}>
+            Maybe you too have your own story to tell, But think you’ve seen it
+            all?{" "}
+          </motion.p>
+          <motion.p className="font-medium lg:text-3xl" variants={fadeInUp}>
+            Meet -{" "}
+          </motion.p>
+        </div>
+      </motion.section>
     </div>
   );
 }
