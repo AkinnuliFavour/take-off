7efb24e5cb5dc81a5adb41b4f5566dd32750bb14
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-07-06 17:52:55 +0100

    refactor: Improve Footer component layout and styling for better responsiveness and user experience

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -13,21 +13,21 @@ export default function Footer() {
       whileInView="visible"
       viewport={{ once: true, amount: 0.3 }}
     >
-      <motion.div className="w-full px-4" variants={fadeInUp}>
-        <p className="flex items-center border-b mt-[166px] gap-1 pb-2 text-center justify-center text-base font-medium text-[#00FF11] lg:border-0">
+      <motion.div className="w-full px-4 lg:pl-[145px]" variants={fadeInUp}>
+        <p className="flex items-center border-b mt-[166px] gap-1 pb-2 justify-center text-base font-medium text-[#00FF11] lg:text-2xl lg:border-0 xl:font-semibold xl:text-[50px] xl:justify-start">
           <Image
             src="/images/logo.svg"
             alt="Logo Icon"
             width={100}
             height={100}
-            className="w-[15.4px] h-[19.25px]"
+            className="w-[15.4px] h-[19.25px] xl:hidden"
           />
           Be the first to know when we launch.
         </p>
       </motion.div>
 
       <motion.form
-        className="max-w-md mx-auto space-y-8 mt-[52px] w-full px-4"
+        className="max-w-6xl space-y-8 lg:space-y-16 mt-[52px] w-full px-4 xl:pl-[145px]"
         variants={fadeInUp}
       >
         <div className="relative">
@@ -36,16 +36,16 @@ export default function Footer() {
             id="name"
             name="name"
             required
-            className="peer block w-full border border-[#00FF11] bg-white px-2 py-3 rounded-[7px] text-[12.5px] focus:outline-none focus:border-[#0EC530] transition-colors text-black"
+             className="peer block w-full border border-[#00FF11] bg-white px-4 py-3 xl:py-5 text-[12.5px] lg:text-2xl xl:text-2xl focus:outline-none focus:border-[#0EC530] transition-colors rounded-[7px] text-black"
             placeholder=" "
             autoComplete="off"
           />
           <label
             htmlFor="name"
-            className="absolute left-2 top-1/2 -translate-y-1/2 text-[12.5px] pointer-events-none transition-all duration-200
-                peer-focus:-top-2 peer-focus:text-xs peer-focus:text-[#00FF11]
-                peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-placeholder-shown:text-[#0EC530] peer-placeholder-shown:text-[12.5px]
-                peer-not-placeholder-shown:-top-2 peer-not-placeholder-shown:text-xs peer-not-placeholder-shown:text-[#00FF11] peer-placeholder-shown:font-semibold"
+           className="absolute left-4 top-1/2 -translate-y-1/2 text-[12.5px] pointer-events-none transition-all duration-200
+                peer-focus:-top-6 peer-focus:text-lg xl:peer-focus-2xl peer-focus:text-[#00FF11]
+                peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-placeholder-shown:text-[#0EC530] peer-placeholder-shown:text-[12.5px] lg:peer-placeholder-shown:text-2xl xl:peer-placeholder-shown:text-[32.13px]
+                peer-not-placeholder-shown:-top-2 peer-not-placeholder-shown:text-[12.5px] peer-not-placeholder-shown:text-[#00FF11] peer-placeholder-shown:font-semibold"
           >
             Enter Name
           </label>
@@ -56,16 +56,16 @@ export default function Footer() {
             id="email"
             name="email"
             required
-            className="peer block w-full border border-[#00FF11] bg-white px-2 py-3 text-[12.5px] focus:outline-none focus:border-[#0EC530] transition-colors rounded-[7px] text-black"
+            className="peer block w-full border border-[#00FF11] bg-white px-4 py-3 xl:py-5 text-[12.5px] lg:text-2xl xl:text-2xl focus:outline-none focus:border-[#0EC530] transition-colors rounded-[7px] text-black"
             placeholder=" "
             autoComplete="off"
           />
           <label
             htmlFor="email"
-            className="absolute left-2 top-1/2 -translate-y-1/2 text-[12.5px] pointer-events-none transition-all duration-200
-                peer-focus:-top-2 peer-focus:text-xs peer-focus:text-[#00FF11]
-                peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-placeholder-shown:text-[#0EC530] peer-placeholder-shown:text-[12.5px]
-                peer-not-placeholder-shown:-top-2 peer-not-placeholder-shown:text-xs peer-not-placeholder-shown:text-[#00FF11] peer-placeholder-shown:font-semibold"
+            className="absolute left-4 top-1/2 -translate-y-1/2 text-[12.5px] pointer-events-none transition-all duration-200
+                peer-focus:-top-6 peer-focus:text-lg xl:peer-focus-2xl peer-focus:text-[#00FF11]
+                peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-placeholder-shown:text-[#0EC530] peer-placeholder-shown:text-[12.5px] lg:peer-placeholder-shown:text-2xl xl:peer-placeholder-shown:text-[32.13px]
+                peer-not-placeholder-shown:-top-2 peer-not-placeholder-shown:text-[12.5px] peer-not-placeholder-shown:text-[#00FF11] peer-placeholder-shown:font-semibold"
           >
             Email
           </label>
@@ -74,36 +74,36 @@ export default function Footer() {
           className="w-full mx-auto flex items-center justify-center hover:cursor-pointer hover:opacity-90"
           variants={scaleIn}
         >
-          <button className="mt-[71px] bg-[#00FF11] font-bold text-base rounded-[10px] py-2 px-4">
+          <button className="mt-[71px] bg-[#00FF11] font-bold text-base rounded-[10px] py-2 px-4 xl:text-[39px] lg:text-2xl">
             Join Waitlist
           </button>
         </motion.div>
       </motion.form>
 
       <motion.div
-        className="flex flex-col gap-5 mt-[91px] bg-[url('/images/footer-bg-img.svg')] bg-cover bg-no-repeat bg-center -bottom-[20rem] right-0 w-full h-[214px]"
+        className="flex flex-col gap-5 mt-[91px] bg-[url('/images/footer-bg-img.svg')] bg-cover bg-no-repeat bg-center -bottom-[20rem] right-0 w-full h-[214px] xl:h-[653px] xl:pl-[145px]"
         variants={fadeInUp}
       >
         <motion.div className="w-full px-4" variants={fadeInUp}>
-          <p className="flex items-center border-b gap-1 pb-2 text-center text-base font-medium text-[#00FF11] pt-4">
+          <p className="flex items-center border-b gap-1 pb-2 text-center text-base font-medium text-[#00FF11] pt-4 lg:text-2xl lg:border-none xl:text-[50px]">
             <Image
               src="/images/logo.svg"
               alt="Logo Icon"
               width={100}
               height={100}
-              className="w-[15.4px] h-[19.25px]"
+              className="w-[15.4px] h-[19.25px] lg:hidden"
             />
             Get in touch.
           </p>
         </motion.div>
 
-        <motion.div className="flex gap-4 text-2xl px-4" variants={scaleIn}>
+        <motion.div className="flex gap-4 lg:gap-8 text-2xl px-4 *:xl:h-[62px] *:xl:w-[62px]" variants={scaleIn}>
           <SiInstagram />
           <SiWhatsapp />
           <SiGmail />
         </motion.div>
         <motion.p
-          className="font-extrabold text-[40px] leading-[66px] flex items-center absolute bottom-0 right-0"
+          className="font-extrabold text-[40px] leading-[66px] flex items-center absolute bottom-0 right-0 xl:text-[160px]"
           variants={fadeIn}
         >
           <Image
@@ -111,7 +111,7 @@ export default function Footer() {
             alt="Logo Image"
             width={100}
             height={100}
-            className="w-[29.15px] h-[36.44px]"
+            className="w-[29.15px] h-[36.44px] xl:h-[263px] xl:w-[263px]"
           />
           RoomRadar
         </motion.p>
